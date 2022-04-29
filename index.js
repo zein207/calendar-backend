ff43459bd64fd9afa4ad2fd18f62c8d891c919eb
David <zein207@gmail.com>
2022-04-28 20:58:32 -0500

    Initial config

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 
+const express = require('express');
 
-console.log('Hello world!!!');
+// Create express server
+const app = express();
+
+// Routes
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+})
+
+// Listen to requests
+app.listen( 4000, () => {
+    console.log(`Server run on port ${ 4000 }`)
+});
