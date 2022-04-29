a9231fe2875f27fd755e9707b5128d3323e08de9
David <zein207@gmail.com>
2022-04-28 21:33:07 -0500

    Endpoints renew, login, create

---
+++ controllers/auth.js
@@ -0,0 +1,29 @@
+const { response } = require('express');
+
+
+const createUser = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'register'
+    });
+}
+
+const loginUser = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'login'
+    })
+}
+
+const renewToken = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'renew'
+    })
+}
+
+module.exports = {
+    createUser,
+    loginUser,
+    renewToken
+}
--- routes/auth.js
+++ routes/auth.js
@@ -6,10 +6,13 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        ok: true
-    })
-})
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
+
+
+router.post('/new', createUser)
+
+router.post('/', loginUser)
+
+router.get('/renew', renewToken)
 
 module.exports = router;
