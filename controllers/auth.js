e60714e3ebcb1a384fa97b6ea02395e303a830f6
David <zein207@gmail.com>
2022-04-29 17:32:06 -0500

    Validate user

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,17 +4,27 @@ const User = require('../models/User');
 
 const createUser = async (req, res = response) => {
 
-    // const { name, email, password } = req.body;
+    const { email, password } = req.body;
 
     try {
 
-        const user = new User( req.body );
+        let user = await User.findOne({ email });
+
+        if( user ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'User already exists'
+            })
+        }
+
+        user = new User( req.body );
     
         await user.save();
     
         res.status(201).json({
             ok: true,
-            msg: 'register'
+            uid: user.id,
+            name: user.name
         });
         
     } catch (error) {
