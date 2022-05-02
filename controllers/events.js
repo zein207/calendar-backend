9448b59d50bfd81b356ed8b60e2068ed7d13d632
David <zein207@gmail.com>
2022-05-02 16:25:35 -0500

    Save event in DB

--- controllers/events.js
+++ controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Event = require('../models/Event');
 
 const getEvents = (req, res = response) => {
 
@@ -8,14 +9,30 @@ const getEvents = (req, res = response) => {
     })
 }
 
-const createEvent = (req, res = response) => {
+const createEvent = async(req, res = response) => {
 
-    console.log( req.body );
+    const event = new Event( req.body );
 
-    res.json({
-        ok: true,
-        msg: 'createEvent'
-    })
+    try {
+
+        event.user = req.uid;
+
+        const savedEvent = await event.save();
+
+        res.json({
+            ok: true,
+            event: savedEvent
+        })
+        
+    } catch (error) {
+        
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact admin'
+        });
+    }
+
 }
 
 const updateEvent = (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -22,7 +22,7 @@ router.post(
     [
         check('title', 'Title is required').not().isEmpty(),
         check('start', 'Start date is required').custom( isDate ),
-        check('title', 'End date is required').custom( isDate ),
+        check('end', 'End date is required').custom( isDate ),
         fieldValidator
     ],
     createEvent
