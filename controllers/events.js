652ccab77004ca4abe3ea36ed59916896e716533
David <zein207@gmail.com>
2022-05-01 21:12:45 -0500

    Event Model

--- controllers/events.js
+++ controllers/events.js
@@ -10,6 +10,8 @@ const getEvents = (req, res = response) => {
 
 const createEvent = (req, res = response) => {
 
+    console.log( req.body );
+
     res.json({
         ok: true,
         msg: 'createEvent'
